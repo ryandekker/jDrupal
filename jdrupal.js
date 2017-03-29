3900d52018754454cd686bc914ed79157fabfa11
Ryan Dekker <ryan@clikfocus.com>
2017-03-28 18:33:57 -0600

    Now with aggregated versions...

--- jdrupal.js
+++ jdrupal.js
@@ -562,7 +562,7 @@ jDrupal.Views.prototype.getView = function() {
     };
     req.open('GET', jDrupal.restPath() + self.getPath()) + '_format=json';
 
-    // Allow for OAuth authorization (if in use)
+    // Allow for OAuth authorization (if in use).
     var oauthToken = jDrupal.oauthToken();
     if (oauthToken) {
       req.setRequestHeader('Authorization', 'Bearer ' + oauthToken);
@@ -859,7 +859,7 @@ jDrupal.Entity.prototype.save = function() {
 
         req.onload = function() {
           _entity.postSave(req).then(function() {
-            if (req.status == 200) {
+            if (req.status == 200 || req.status == 201) {
               var invoke = jDrupal.moduleInvokeAll('rest_post_process', req);
               if (!invoke) { resolve(); }
               else { invoke.then(resolve); }
@@ -888,13 +888,13 @@ jDrupal.Entity.prototype.postSave = function(xhr) {
   var self = this;
   return new Promise(function(resolve, reject) {
     // For new entities, grab their id from the Location response header.
-    if (self.isNew()) {
-      var parts = xhr.getResponseHeader('Location').split('/');
-      var entityID =
-        self.entity[self.getEntityKey('id')] = [ {
-          value: parts[parts.length - 1]
-        }];
-    }
+    // if (self.isNew()) {
+    //   var parts = xhr.getResponseHeader('Location').split('/');
+    //   var entityID =
+    //     self.entity[self.getEntityKey('id')] = [ {
+    //       value: parts[parts.length - 1]
+    //     }];
+    // }
     resolve();
   });
 };
